3485174a1e2ace7bcb85dfa0c8fb2ae81c23daaa
emaema1128 <y.on.dw3@gmail.com>
2024-03-05 22:11:30 +0900

    ルーティングエラー修正とビュー修正

--- db/migrate/20240222145632_create_chatbots.rb
+++ db/migrate/20240222145632_create_chatbots.rb
@@ -2,7 +2,6 @@ class CreateChatbots < ActiveRecord::Migration[7.0]
   def change
     create_table :chatbots do |t|
       t.text :text
-
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_02_22_145632) do
   create_table "chatbots", charset: "utf8", force: :cascade do |t|
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
